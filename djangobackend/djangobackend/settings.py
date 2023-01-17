ab9ce2f41c26f31e4ef10a0360f075a1a439262c
aadityasinha-dotcom <aadityasinha009@gmail.com>
2023-01-17 21:25:30 +0530

    changes

--- djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-i=)_8r1qht=e(80+k=)@&1o=x=un!=39utg@ld3t*v$l!su5_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
 
 # Application definition
